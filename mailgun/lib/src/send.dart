79c2f0799b8bd907526047fa85d90a683cbc563d
kyorohiro <kyorohiro@gmail.com>
2017-03-26 02:33:50 +0900

    ..

--- mailgun/lib/src/send.dart
+++ mailgun/lib/src/send.dart
@@ -9,26 +9,6 @@ class SendBox {
   SendBox(this.builder, this.config) {
   }
 
-  Future<pro.MiniProp> route(List<String> actions,{
-    int priority:0,
-    String description:"default",
-    String expression: "catch_all()",
-    }) async {
-    String url = "https://api.mailgun.net/v3/routes";
-    req.Requester requester = await this.builder.createRequester();
-    req.Multipart multipart = new req.Multipart();
-    multipart.add(new req.MultipartPlainText.fromTextPlain("priority", "${priority}"));
-    multipart.add(new req.MultipartPlainText.fromTextPlain("description", "${description}"));
-    multipart.add(new req.MultipartPlainText.fromTextPlain("expression", "${expression}"));
-    for(String action in actions) {
-      multipart.add(new req.MultipartPlainText.fromTextPlain("action", "${action}"));
-    }
-    req.Response response = await await multipart.post(requester, url,
-    headers:<String,String>{"Authorization": "Basic "+conv.BASE64.encode(conv.UTF8.encode("api:"+config.secretAPIKey)) });
-    print("xxs${response.status}");
-    return new pro.MiniProp.fromByte(response.response.asUint8List(), errorIsThrow: false);
-  }
-
 /*
 curl -s --user 'api:YOUR_API_KEY' -G \
     https://api.mailgun.net/v3/YOUR_DOMAIN_NAME/events
